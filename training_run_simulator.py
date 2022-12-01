d408848dda92d74fde75ec9ada484367bde0f35b
Aaron Wong <aaronwong668@gmail.com>
2022-12-01 11:26:57 +0800

    added worst smooth worst compact and worst concave

--- training_run_simulator.py
+++ training_run_simulator.py
@@ -47,7 +47,8 @@ features = ['mean_radius', 'mean_texture', 'mean_perimeter', 'mean_area',
        'radius_error', 'texture_error','perimeter_error', 'area_error',
        'smoothness_error', 'compactness_error', 'concavity_error',
        'concave_points_error','symmetry_error', 'fractal_dimension_error',
-       'worst_radius','worst_texture', 'worst_perimeter', 'worst_area']
+       'worst_radius','worst_texture', 'worst_perimeter', 'worst_area',
+       'worst_smoothness', 'worst_compactness', 'worst_concavity']
 
 filepath = Path.cwd().joinpath('./datasets/features/features.csv')
 filepath.parent.mkdir(parents=True, exist_ok=True)
