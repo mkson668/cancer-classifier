a487dcab1b4005b2fea85b7c29edbd266ca06cf8
Aaron Wong <aaronwong668@gmail.com>
2022-12-01 11:14:53 +0800

    added radius error and texture error

--- training_run_simulator.py
+++ training_run_simulator.py
@@ -43,7 +43,8 @@ X.columns = [c.lower().replace(' ', '_') for c in X.columns]
 
 features = ['mean_radius', 'mean_texture', 'mean_perimeter', 'mean_area',
        'mean_smoothness', 'mean_compactness', 'mean_concavity',
-       'mean_concave_points', 'mean_symmetry', 'mean_fractal_dimension']
+       'mean_concave_points', 'mean_symmetry', 'mean_fractal_dimension',
+       'radius_error', 'texture_error',]
 
 filepath = Path.cwd().joinpath('./datasets/features/features.csv')
 filepath.parent.mkdir(parents=True, exist_ok=True)
