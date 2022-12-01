48c73c8ae3667ee78725fefb0900860f2d571759
Aaron Wong <aaronwong668@gmail.com>
2022-12-01 11:19:16 +0800

    added compact error, convacity error, and concave point error

--- training_run_simulator.py
+++ training_run_simulator.py
@@ -45,7 +45,8 @@ features = ['mean_radius', 'mean_texture', 'mean_perimeter', 'mean_area',
        'mean_smoothness', 'mean_compactness', 'mean_concavity',
        'mean_concave_points', 'mean_symmetry', 'mean_fractal_dimension',
        'radius_error', 'texture_error','perimeter_error', 'area_error',
-       'smoothness_error']
+       'smoothness_error', 'compactness_error', 'concavity_error',
+       'concave_points_error']
 
 filepath = Path.cwd().joinpath('./datasets/features/features.csv')
 filepath.parent.mkdir(parents=True, exist_ok=True)
